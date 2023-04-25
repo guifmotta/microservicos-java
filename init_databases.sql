15a01351e838b66e9e81a82f596e8cea75deba17
guifmotta <guifmotta@gmail.com>
2023-04-24 23:19:18 -0300

    Inserção do script de criação inicial dos bancos de dados

---
+++ init_databases.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE pagamento
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
+CREATE DATABASE pedidos
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
